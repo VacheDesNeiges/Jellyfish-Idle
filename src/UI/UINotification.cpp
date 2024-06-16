db11dc884673420bfeef0ce0d8f0e69aa97ebae4
Dindon <vachedesneiges@gmail.com>
2024-06-16 21:41:18 +0200

    Notification popups are now using modal windows

--- src/UI/UINotification.cpp
+++ src/UI/UINotification.cpp
@@ -7,11 +7,16 @@ UINotification::render () const
   if (auto notif = gData->getAchievementsView ()->getNextNotification ();
       notif.has_value ())
     {
-      ImGui::Begin ("Notification", nullptr, ImGuiWindowFlags_None);
-      ImGui::SetWindowFocus ();
-      ImGui::Text ("%s", std::string (notif.value ()).c_str ());
-      if (ImGui::Button ("close"))
-        inputHandler->removeCurrentNotification ();
-      ImGui::End ();
+      ImGui::OpenPopup ("Notification");
+      ImGui::SetNextWindowSize (ImGui::GetMainViewport ()->GetCenter ());
+      if (ImGui::BeginPopupModal ("Notification", nullptr,
+                                  ImGuiWindowFlags_None))
+        {
+          ImGui::Text ("%s", std::string (notif.value ()).c_str ());
+          if (ImGui::Button ("close"))
+            inputHandler->removeCurrentNotification ();
+
+          ImGui::EndPopup ();
+        }
     }
 }
