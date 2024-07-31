d712c6afab9bbf1abe6ae0cfcb919af77fb6af7c
Dindon <vachedesneiges@gmail.com>
2024-07-31 09:44:07 +0200

    added exception handling for quest subsystem

--- src/Quests/Quest.cpp
+++ src/Quests/Quest.cpp
@@ -1,16 +1,27 @@
 #include "Quest.hpp"
 #include "GameIDsTypes.hpp"
+#include <cstdlib>
+#include <iostream>
+#include <nlohmann/json_fwd.hpp>
 #include <vector>
 
 Quest::Quest (const nlohmann::json &json)
 {
-  dialogue = json.at ("Text");
-  requestedRessources.reserve (json.at ("CompletionRequirements").size ());
-  for (const auto &requirements : json.at ("CompletionRequirements"))
+  try
     {
-      requestedRessources.emplace_back (
-          RessourceType (requirements.at ("RessourceID")),
-          requirements.at ("Quantity"));
+      dialogue = json.at ("Text");
+      requestedRessources.reserve (json.at ("CompletionRequirements").size ());
+      for (const auto &requirements : json.at ("CompletionRequirements"))
+        {
+          requestedRessources.emplace_back (
+              RessourceType (requirements.at ("RessourceID")),
+              requirements.at ("Quantity"));
+        }
+    }
+  catch (nlohmann::json::exception &e)
+    {
+      std::cerr << "error while parsing a quest :\n" << e.what () << '\n';
+      abort ();
     }
 }
 
--- src/Quests/Questline.cpp
+++ src/Quests/Questline.cpp
@@ -1,15 +1,25 @@
 #include "Questline.hpp"
 #include "GameIDsTypes.hpp"
+#include <iostream>
 #include <nlohmann/json.hpp>
+#include <nlohmann/json_fwd.hpp>
 #include <utility>
 #include <vector>
 
 Questline::Questline (const nlohmann::json &json)
 {
-  quests.reserve (json.at ("Quests").size ());
-  for (const auto &quest : json.at ("Quests"))
+  try
     {
-      quests.emplace_back (quest);
+      quests.reserve (json.at ("Quests").size ());
+      for (const auto &quest : json.at ("Quests"))
+        {
+          quests.emplace_back (quest);
+        }
+    }
+  catch (nlohmann::json::exception &e)
+    {
+      std::cerr << "Error while parsing a questline :\n" << e.what () << '\n';
+      abort ();
     }
 }
 
